b0fbe40e8c7f320356f4fbb8f84f8af149608eb4
devotetime <wz-wangzhen@outlook.com>
2026-08-06 11:53:22 +0000

    去除偏航数据

--- src/train/train.sh
+++ src/train/train.sh
@@ -7,7 +7,7 @@ cd "$SCRIPT_DIR"
 echo "Switched to directory: $SCRIPT_DIR"
 
 CONFIG_PATH="config/config.yaml"
-OUTPUT_DIR="/workspace/data2/model/ablation_new/fds_ids/panovggt_0.20_lr2e-5_singlepoint_ids_8card"
+OUTPUT_DIR="/workspace/data2/model/ablation_new/panovggt_pre_merger/panovggt_0.20_lr2e-5_singlepoint_8card"
 GPU_DEVICES="0,1,2,3,4,5,6,7"
 GPU_NUM="$(awk -F',' '{print NF}' <<< "$GPU_DEVICES")"
 MASTER_ADDR="127.0.0.1"
